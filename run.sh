8af9718dc09927f593dba3b113aca30c87022527
PapyrusThePlant <papyrustheplant@gmail.com>
2016-07-24 12:28:25 +0200

    Update option, starts the bot in the bg

--- run.sh
+++ run.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-python3 -B main.py --logs logs/scarecrow.log
+print_help () {
+  echo -e "Usage :\n    $0 [update]"
+}
+
+if [ $# -gt 1 ]
+then
+  print_help
+  exit 1
+fi
+
+if [ "$1" == 'update' ]
+then
+  echo Updating requirements...
+  git pull
+  python3 -m pip install --user -U git+https://github.com/Rapptz/discord.py
+  exit 0
+elif [ "$1" != '' ]
+then
+  print_help
+  exit 1
+fi
+
+echo Starting bot...
+python3 -B main.py --logs logs/scarecrow.log &
 
