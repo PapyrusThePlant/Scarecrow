1aca9ae939361dd34db1e1d9723089c37bdc5a23
PapyrusThePlant <papyrustheplant@gmail.com>
2017-01-10 21:37:34 +0100

    Added a magical 8 ball command for Shimmy's server...

--- cogs/shimmy.py
+++ cogs/shimmy.py
@@ -1,3 +1,5 @@
+import random
+
 import discord
 import discord.ext.commands as commands
 
@@ -5,6 +7,33 @@ from .util import checks
 
 SHIMMY_SERVER_ID = '140880261360517120'
 NSFW_ROLE_ID = '261189004681019392'
+
+
+eight_ball_responses = [
+    # Positive
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    # Non cmmmittal
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    # Negative
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+]
 
 
 def setup(bot):
@@ -23,3 +52,8 @@ class Shimmy:
         await self.bot.add_roles(ctx.message.author, discord.Object(id=NSFW_ROLE_ID))
         await self.bot.say('\N{WHITE HEAVY CHECK MARK} Access granted.', delete_after=3)
         await self.bot.delete_message(ctx.message)
+
+    @commands.command(aliases=['eight', '8'])
+    @checks.in_server(SHIMMY_SERVER_ID)
+    async def ball(self, *, question):
+        await self.bot.say(random.choice(eight_ball_responses))
