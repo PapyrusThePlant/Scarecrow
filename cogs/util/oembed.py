65d11438a0771e7fe9945fbbd9d3f6a962d13eb8
PapyrusThePlant <papyrustheplant@gmail.com>
2017-03-21 17:43:11 +0100

    Create the oEmbed discovery list on load.

--- cogs/util/oembed.py
+++ cogs/util/oembed.py
@@ -8,11 +8,16 @@ from . import utils
 
 with open(paths.OEMBED_PROVIDERS, 'r', encoding='utf-8') as fp:
     oEmbed_providers = json.load(fp)
+    oEmbed_discovery = []
 
     # Escaoe the special characters in the schemes
     for provider in oEmbed_providers:
         for endpoint in provider['endpoints']:
             endpoint['schemes'] = [s.replace('.', '\.').replace('?', '\?') for s in endpoint.get('schemes', [])]
+            if not endpoint['schemes'] and endpoint.get('discovery', False):
+                oEmbed_discovery.append(endpoint['url'])
+
+    oEmbed_discovery.reverse()  # Yes, bleh, whatever, get over it
 
 
 class OembedException(Exception):
@@ -32,39 +37,36 @@ class NoOembedData(OembedException):
 
 
 def find_oembed_endpoint(url):
-    discovery = []
     for provider in oEmbed_providers:
         for endpoint in provider['endpoints']:
-            if not endpoint['schemes'] and endpoint.get('discovery', False):
-                discovery.append(endpoint['url'])
             for scheme in endpoint['schemes']:
                 if re.match(scheme.replace('//www.', '//'), url.replace('//www.', '//')):
-                    return endpoint['url'], None
+                    return endpoint['url']
 
-    if not discovery:
-        raise EndpointNotFound(url)
+    raise EndpointNotFound(url)
 
-    discovery.reverse()  # Yes, bleh, whatever, get over it
-    return None, discovery
 
 async def fetch_oembed_data(url):
-    endpoint, discovery = find_oembed_endpoint(url)
+    data = None
 
-    if endpoint is not None:
-        try:
-            return await utils.fetch_page(endpoint, params={'url': url, 'format': 'json'})
-        except:
-            return await utils.fetch_page(endpoint, data={'url': url, 'format': 'json'})
-    else:
-        for endpoint_url in discovery:
+    try:
+        endpoint = find_oembed_endpoint(url)
+    except EndpointNotFound:
+        for endpoint_url in oEmbed_discovery:
             try:
                 data = await utils.fetch_page(endpoint_url, params={'url': url, 'format': 'json'})
             except:
                 try:
                     data = await utils.fetch_page(endpoint_url, data={'url': url, 'format': 'json'})
                 except:
-                    data = None
-            if isinstance(data, dict):
-                return data
+                    pass
+    else:
+        try:
+            data = await utils.fetch_page(endpoint, params={'url': url, 'format': 'json'})
+        except:
+            data = await utils.fetch_page(endpoint, data={'url': url, 'format': 'json'})
 
-        raise NoOembedData(url)
+    if isinstance(data, dict):
+        return data
+
+    raise NoOembedData(url)
