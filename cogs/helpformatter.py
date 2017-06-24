27081a279d1c6e2f83885bb174351784cbf7f2b3
PapyrusThePlant <papyrustheplant@gmail.com>
2017-06-25 00:08:00 +0200

    HelpFormatter: Hide subcommands that can't be run.

--- cogs/helpformatter.py
+++ cogs/helpformatter.py
@@ -1,5 +1,9 @@
+import inspect
+import itertools
+
+import discord
 import discord.ext.commands as commands
-from discord.ext.commands.core import GroupMixin
+from discord.ext.commands.core import GroupMixin, Command
 
 
 def setup(bot):
@@ -16,12 +20,12 @@ class HelpFormatter(commands.HelpFormatter):
         super().__init__(*args, **kwargs)
         self.base_indent = '   '
 
-    def _add_subcommands_to_page(self, max_width, commands, base_indent=None):
+    async def _add_subcommands_to_page(self, max_width, commands, base_indent=None):
         """Adds commands/subcommands and their description to the paginator."""
         iterator = iter(sorted(commands))
         for name, command in iterator:
-            if name in command.aliases:
-                # skip aliases
+            if name in command.aliases or not await command.can_run(self.context):
+                # skip aliases and commands the user can't run
                 continue
 
             # Ugly indent shenanigans
@@ -39,7 +43,7 @@ class HelpFormatter(commands.HelpFormatter):
 
             if isinstance(command, GroupMixin):
                 indent = indent.replace('\u251c\u2500', '\u2502 ').replace('\u2514\u2500', '  ')  # wew wth
-                self._add_subcommands_to_page(max_width, command.all_commands.items(), indent)
+                await self._add_subcommands_to_page(max_width, command.all_commands.items(), indent)
 
     def _get_max_width(self, command, depth=1):
         """Tricks and ponies to get the appropriate max_width."""
@@ -60,3 +64,67 @@ class HelpFormatter(commands.HelpFormatter):
             return 0
         except AttributeError:
             return len(self.command.name)
+
+    async def format(self):
+        """Handles the actual behaviour involved with formatting.
+
+        To change the behaviour, this method should be overridden.
+
+        Returns
+        --------
+        list
+            A paginated output of the help command.
+        """
+        self._paginator = discord.ext.commands.Paginator()
+
+        # we need a padding of ~80 or so
+
+        description = self.command.description if not self.is_cog() else inspect.getdoc(self.command)
+
+        if description:
+            # <description> portion
+            self._paginator.add_line(description, empty=True)
+
+        if isinstance(self.command, Command):
+            # <signature portion>
+            signature = self.get_command_signature()
+            self._paginator.add_line(signature, empty=True)
+
+            # <long doc> section
+            if self.command.help:
+                self._paginator.add_line(self.command.help, empty=True)
+
+            # end it here if it's just a regular command
+            if not self.has_subcommands():
+                self._paginator.close_page()
+                return self._paginator.pages
+
+        max_width = self.max_name_size
+
+        def category(tup):
+            cog = tup[1].cog_name
+            # we insert the zero width space there to give it approximate
+            # last place sorting position.
+            return cog + ':' if cog is not None else '\u200bNo Category:'
+
+        filtered = await self.filter_command_list()
+        if self.is_bot():
+            data = sorted(filtered, key=category)
+            for category, commands in itertools.groupby(data, key=category):
+                # there simply is no prettier way of doing this.
+                commands = sorted(commands)
+                if len(commands) > 0:
+                    self._paginator.add_line(category)
+
+                await self._add_subcommands_to_page(max_width, commands)
+        else:
+            filtered = sorted(filtered)
+            if filtered:
+                self._paginator.add_line('Commands:')
+                await self._add_subcommands_to_page(max_width, filtered)
+
+        # add the ending note
+        self._paginator.add_line()
+        ending_note = self.get_ending_note()
+        self._paginator.add_line(ending_note)
+        return self._paginator.pages
