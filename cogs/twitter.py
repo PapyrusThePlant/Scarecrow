33cad1d9986b07ee2c821180d823fe523e427619
PapyrusThePlant <papyrustheplant@gmail.com>
2018-01-18 09:24:26 +0100

    Twitter: Fix hourglass not being removed in €twitter fetch.

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -254,12 +254,12 @@ class Twitter:
                     except discord.NotFound:
                         pass # The user probably deleted his message before we tried to do it
                 else:
-                    await ctx.message.remove_reaction('\N{HOURGLASS}', ctx.me)
+                    await ctx.message.remove_reaction('\N{HOURGLASS WITH FLOWING SAND}', ctx.me)
                     await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
 
     @twitter_fetch.error
     async def twitter_fetch_error(self, ctx, error):
-        await ctx.message.remove_reaction('\N{HOURGLASS}', ctx.me)
+        await ctx.message.remove_reaction('\N{HOURGLASS WITH FLOWING SAND}', ctx.me)
 
     @twitter_group.command(name='follow')
     @commands.guild_only()
